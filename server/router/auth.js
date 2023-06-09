42ac94a45d0b21c968fe87a857f6021ff34572cb
mehargee <mehrgee826@gmail.com>
2023-06-09 22:36:18 +0500

    12video

--- server/router/auth.js
+++ server/router/auth.js
@@ -10,19 +10,29 @@ router.get('/', (req, res) => {
 router.post('/register', async (req, res) => {
 
     const { name, email, phone, work, password, cpassword } = req.body;
-    
+
     if (!name || !email || !phone || !work || !password || !cpassword) {
         return res.status(422).json({ error: "filled the all field" })
     }
 
     try {
-        const userExist = await User.findOne({ email: email })
-        if (userExist) {
-            return res.status(422).json("Email already exist");
-        }
 
-        const user = new User({ name, email, phone, work, password, cpassword }); //key:value
-        await user.save();
+        const userExist = await User.findOne({ email: email }) //is email ka sara data aa gya ha
+        if (userExist) {
+
+            return res.status(422).json({error: "Email already exist"});
+        }
+        else if (password != cpassword) {
+            return res.status(422).json({error: "password are not matching"});
+        }
+        else {
+            const user = new User({ name, email, phone, work, password, cpassword }); //key:value
+
+            await user.save();
+
+            res.status(201).json({ message: "user register sucessfully" });
+            console.log("user register sucessfully..");
+        }
 
     }
     catch (error) {
@@ -32,5 +42,38 @@ router.post('/register', async (req, res) => {
 
 });
 
+router.post('/signin', async (req, res) => {
+
+    const { email, password } = req.body;
+    // const email = req.body.email;
+    // const password = req.body.password;
+
+    try {
+        if (!email || !password) {
+            return res.status(400).json({error: "fill the both fields"});
+        }
+
+        const userLogin = await User.findOne({ email: email })
+
+        if (!userLogin) {
+
+            return res.status(400).json({ error: "user invalid" })
+
+        } else {
+
+            return res.status(200).json({ message: "user login sucessfully" })
+            console.log("user login sucessfully");
+        }
+
+
+
+
+    } catch (error) {
+        console.log("error", error);
+        res.status(500).json({ error: "error" })
+    }
+
+
+});
 
 module.exports = router;
